c392af3b34c7a2234cf9ff80dbaaf2658a787683
BobKoto <110218823+BobKoto@users.noreply.github.com>
2023-03-30 01:42:43 -0400

    Update basicobject.js

--- scripts/basicobject.js
+++ scripts/basicobject.js
@@ -1,14 +1,14 @@
 // JavaScript source code
 
+
+function Person(name) {
 const title = document.getElementById('title');
 const text = document.getElementById('text');
 const button = document.getElementById('btn');
-
-function Person(name) {
   this.name = name;
   this.introduceSelf = function () {
     console.log(`Hi! I'm ${this.name}.`);
-    text.textContent += ' Hi! Im' ;
+   // text.textContent += ' Hi! Im' ;
     text.textContent +=  this.name;
    // document.write ${this.name};
   };
@@ -23,6 +23,7 @@ frankie.name;
 frankie.introduceSelf();
 // "Hi! I'm Frankie."
 // Get the elements you want to manipulate
+
 
 
 // Change the text of the <h1> element
