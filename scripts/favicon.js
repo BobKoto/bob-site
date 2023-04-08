bd7ace2f59efc013132877000f4d2d0690b9a996
BobKoto <110218823+BobKoto@users.noreply.github.com>
2023-04-08 00:48:31 -0400

    add script; apply in index.md

---
+++ scripts/favicon.js
@@ -0,0 +1,6 @@
+
+  var link = document.createElement('link');
+  link.rel = 'shortcut icon';
+  link.type = 'image/png';
+  link.href = 'https://bobkoto.github.io/bob-site/favicon.png';
+  document.head.appendChild(link);
