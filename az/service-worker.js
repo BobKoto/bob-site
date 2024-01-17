5fbdcbfca3cc9be90ce30d0ab5feb89e6248cd68
BobKoto <110218823+BobKoto@users.noreply.github.com>
2024-01-17 00:51:22 -0500

    service worker try

--- az/service-worker.js
+++ az/service-worker.js
@@ -1,12 +1,17 @@
 // Service Worker
-
+var GHPATH = '/bob-site/az';
 const CACHE_NAME = 'hello-pwa-cache-v146';
+var APP_PREFIX = 'hellopwa_';
 const urlsToCache = [
-  "https://bobkoto.github.io/bob-site/az/",
-  "https://bobkoto.github.io/bob-site/az/index.html",
-  "https://bobkoto.github.io/bob-site/az/azmanifest.json",
-  "https://bobkoto.github.io/bob-site/az/icon.png",
+  "${GHPATH}/",
+  "${GHPATH}/index.html",
+  "${GHPATH}/azmanifest.json",
+  "${GHPATH}/icon.png",
 ];
+//"https://bobkoto.github.io/bob-site/az/",
+//"https://bobkoto.github.io/bob-site/az/index.html",
+//"https://bobkoto.github.io/bob-site/az/azmanifest.json",
+//"https://bobkoto.github.io/bob-site/az/icon.png",
 console.log("Service worker location", location);
 
 self.addEventListener("install", (event) => {
@@ -14,10 +19,27 @@ self.addEventListener("install", (event) => {
     (async () => {
       const cache = await caches.open(CACHE_NAME);
       cache.addAll(urlsToCache);
-      console.log("Service worker install event in az 402", urlsToCache);
+      console.log("Service worker install event in az 1245", urlsToCache);
     })(),
   );
 }); 
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('Deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
 
 self.addEventListener('fetch', event => {
   event.respondWith(
@@ -25,3 +47,57 @@ self.addEventListener('fetch', event => {
       .then(response => response || fetch(event.request))
   );
 });
+
+/*
+var GHPATH = '/github-page-pwa';
+var APP_PREFIX = 'gppwa_';
+var VERSION = 'version_002';
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/css/styles.css`,
+  `${GHPATH}/img/icon.png`,
+  `${GHPATH}/js/app.js`
+]
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Installing cache : ' + CACHE_NAME);
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('Deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
+var CACHE_NAME = APP_PREFIX + VERSION
+self.addEventListener('fetch', function (e) {
+  console.log('Fetch request : ' + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { 
+        console.log('Responding with cache : ' + e.request.url);
+        return request
+      } else {       
+        console.log('File is not cached, fetching : ' + e.request.url);
+        return fetch(e.request)
+      }
+    })
+  )
+})
+*/
