9cc7e40da9ac989d201e347e76f810406fbd0368
BobKoto <110218823+BobKoto@users.noreply.github.com>
2024-01-30 03:02:10 -0500

    remove ios/144.png from SWorker cache
    error in file?

--- az/service-worker.js
+++ az/service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker   in most cases be sure to edit VERSION to update/add cached content
-var VERSION = 'version_0a41';    //change index.html too!!! for now
+var VERSION = 'version_0a42';    //change index.html too!!! for now
 var GHPATH = '/bob-site/az';
 const CACHE_NAME = 'hello-pwa-cache-v146';
 var APP_PREFIX = 'hellopwa_';
@@ -133,7 +133,6 @@ const urlsToCache = [
 "https://bobkoto/github.io/bob-site/az/pwaicons/ios/114.png",
 "https://bobkoto/github.io/bob-site/az/pwaicons/ios/120.png",
 "https://bobkoto/github.io/bob-site/az/pwaicons/ios/128.png",
-"https://bobkoto/github.io/bob-site/az/pwaicons/ios/144.png",
 "https://bobkoto/github.io/bob-site/az/pwaicons/ios/152.png",
 "https://bobkoto/github.io/bob-site/az/pwaicons/ios/167.png",
 "https://bobkoto/github.io/bob-site/az/pwaicons/ios/180.png",
@@ -143,6 +142,9 @@ const urlsToCache = [
 "https://bobkoto/github.io/bob-site/az/pwaicons/ios/1024.png",
 
 ];
+
+//"https://bobkoto/github.io/bob-site/az/pwaicons/ios/144.png",     this errored?
+
 // well looks like /az/ does not cache any of its directory contents - gotta spell 'em out?
 //wonder if it's cuz we're working from a subdirectory and not the root of the site... or github pages w/Jekyll?
 
