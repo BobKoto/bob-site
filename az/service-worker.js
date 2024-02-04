56bf2f8f50d1d35da706c8cad553f2b3a1bd5af8
BobKoto <110218823+BobKoto@users.noreply.github.com>
2024-02-04 00:19:03 -0500

    try to cache  pwabuilder icons in a subfolder
    in this case just 6 android icons in az/pwaicons/android/...

--- az/service-worker.js
+++ az/service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker   in most cases be sure to edit VERSION to update/add cached content
-var VERSION = 'version_0a080';    //change index.html too!!! for now
+var VERSION = 'version_0a081';    //change index.html too!!! for now
 var GHPATH = '/bob-site/az';
 const CACHE_NAME = 'hello-pwa-cache-v146';
 var APP_PREFIX = 'hellopwa_';
@@ -16,6 +16,12 @@ const urlsToCache = [
 "https://bobkoto.github.io/bob-site/az/intericon1024.png",
 "https://bobkoto.github.io/bob-site/az/installbutton.png",
 "https://bobkoto.github.io/bob-site/az/android-launchericon-144-144.png",
+"https://bobkoto/github.io/bob-site/az/pwaicons/android/android-launchericon-512-512.png",
+"https://bobkoto/github.io/bob-site/az/pwaicons/android/android-launchericon-192-192.png",
+"https://bobkoto/github.io/bob-site/az/pwaicons/android/android-launchericon-144-144.png",
+"https://bobkoto/github.io/bob-site/az/pwaicons/android/android-launchericon-96-96.png",
+"https://bobkoto/github.io/bob-site/az/pwaicons/android/android-launchericon-72-72.png",
+"https://bobkoto/github.io/bob-site/az/pwaicons/android/android-launchericon-48-48.png",
 "https://bobkoto.github.io/bob-site/az/notnowbutton.png",
 "https://bobkoto.github.io/bob-site/az/rungamebutton.png",
 "https://bobkoto.github.io/bob-site/az/RegisterServiceWorker.js",
