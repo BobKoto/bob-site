ad57d2cb4c2df14d8ed825ed930b8f54eff22977
BobKoto <110218823+BobKoto@users.noreply.github.com>
2024-02-09 02:57:35 -0500

    more con log work
    to track richer install UI not occurring issue

--- az/RegisterServiceWorker.js
+++ az/RegisterServiceWorker.js
@@ -2,49 +2,21 @@
 
 console.log("Service worker registration started:");
 
-        // Register the service worker
-        if ('serviceWorker' in navigator) {
-          navigator.serviceWorker.register('service-worker.js')
-              .then(registration => {
-                  console.log('Service Worker registered with scope:', registration.scope);
-
-                  // Listen for messages from the service worker
-                  navigator.serviceWorker.addEventListener('message', event => {
-                      // Update the message on the page
-                      document.getElementById('message-container').innerText = event.data;
-                      console.log("SW Registrar placing sw message post on page");
-                  });
-              })
-              .catch(error => {
-                  console.error('Service Worker registration failed:', error);
-              });
-      }
-
-/*
-if ("serviceWorker" in navigator) {
-    // declaring scope manually
-    navigator.serviceWorker.register("service-worker.js").then(
-      (registration) => {
-        console.log("Service worker registration succeeded:", registration.scope);
-      },
-      (error) => {
-        console.error(`Service worker registration failed: ${error}`);
-      },
-    );
-  } else {
-    console.error("Service workers are not supported.");
-  }
-
-
+// Register the service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("service-worker.js")
-        navigator.serviceWorker.register("/service-worker.js", { scope: "/az/" }).then(    //scope added
+  navigator.serviceWorker.register('service-worker.js')
+    .then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
 
-   // navigator.serviceWorker.register('/service-worker.js')   //somebody, github pages? dunlike service worker in the root?
-        .then(registration => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(error => {
-            console.error('Service Worker registration failed:', error);
-        });
-} */
+      // Listen for messages from the service worker
+      navigator.serviceWorker.addEventListener('message', event => {
+        // Update the message on the page
+        document.getElementById('message-container').innerText = event.data;
+        console.log("SW Registrar placing sw message post on page");
+      });
+    })
+    .catch(error => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
